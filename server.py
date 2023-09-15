fc6d0fee97b1088d7e96adf15599cc7421ea1b17
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2023-09-16 00:05:40 +0200

    Test with Svelte

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import render_template, request, redirect, Flask, jsonify
+from flask import render_template, request, redirect, Flask, jsonify, send_from_directory
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="frontend/dist")
 
-@app.route("/")
-def main():
-    return render_template("index.html")
+@app.route("/<path:path>")
+def send_report(path):
+    return send_from_directory('frontend/dist', path)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")

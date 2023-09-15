7dc9c68f628a9d5ee456c08e8f37b46b0385a005
Oguz <oemre.kara@gmail.com>
2023-09-16 00:05:07 +0200

    hier ist mein zweiter versuch ein code zu schreiben

--- WebScrapping/newsletter.py
+++ WebScrapping/newsletter.py
@@ -1,25 +1,20 @@
 import re
-from bs4 import BeautifulSoup
 
-# Replace the path with the correct path to your HTML file
-newsletter_path = 'C:\\Users\\Oguz\\PycharmProjects\\jugendhacktköln\\Newsletter.html'
+newsletter_path = r"C:\Users\Oguz\PycharmProjects\MyEventWorld\Newsletter.html"
 
 try:
     with open(newsletter_path, 'r', encoding='utf-8') as file:
         newsletter_content = file.read()
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(newsletter_content, 'html.parser')
 
-    # Define a pattern to match dates (customize it based on your needs)
-    date_pattern = r'\d{1,2}/\d{1,2}/\d{4}'
+    date_pattern = r'Date:\s*(\d{1,2}/\d{1,2}/\d{4})'
+    location_pattern = r'Location:\s*([\w\s]+)'
 
-    # Find all elements containing dates
-    date_elements = soup.find_all(text=re.compile(date_pattern))
+    dates = re.findall(date_pattern, newsletter_content)
+    locations = re.findall(location_pattern, newsletter_content)
 
-    # Extract and print the dates
-    for date in date_elements:
-        print(date.strip())
+    for date, location in zip(dates, locations):
+        print(f"Date: {date}, Location: {location}")
 
 except FileNotFoundError:
     print(f"The file '{newsletter_path}' does not exist.")

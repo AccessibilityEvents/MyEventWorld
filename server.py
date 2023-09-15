750296c294e61b98db02d3563bc10ee88e8b71dd
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-16 00:31:05 +0200

    fix flask + svelte

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import render_template, request, redirect, Flask, jsonify, send_from_directory
+from flask import (
+    render_template,
+    request,
+    redirect,
+    Flask,
+    jsonify,
+    send_from_directory,
+)
 
-app = Flask(__name__, template_folder="frontend/dist")
+app = Flask(__name__, template_folder="frontend/dist", static_url_path="/", static_folder='frontend/dist/')
 
-@app.route("/<path:path>")
-def send_report(path):
-    return send_from_directory('frontend/dist', path)
 
-if __name__ == '__main__':
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")

4820bdebea7cdf685de3c4b14ef9ea1f5f3a9bcb
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2023-09-16 15:32:22 +0200

    Added api and more

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from os import path
-import mimetypes
+from flask_cors import CORS
+import suche
 
 from flask import (
     render_template,
@@ -12,18 +13,57 @@ from flask import (
 
 app = Flask(__name__)
 
-#For tests
 @app.route("/")
 def main():
-    return redirect("/api/search", 302)
+    return redirect("/api/all", 304)
 
+@app.route("/api/all")
+def api_all():
+    data = suche.read_data()
+    titles = []
+    times_start = []
+    times_end = []
+    descriptions = []
+    links = []
+    costs = []
+    addresses = []
+    for count in range(len(data)):
+        if count == 2:
+            titles = data[2]
+        elif count == 1:
+            times_end = data[1]
+        elif count == 0:
+            times_start = data[0]
+        elif count == 3:
+            descriptions = data[3]
+        elif count == 4:
+            costs = data[4]
+        elif count == 5:
+            links = data[5]
+        elif count == 6:
+            addresses = data[6]
+
+    res = []
+
+    for num in range(len(titles)):
+        res.append(
+            {"Titel": titles[num], "Start": times_start[num], "Ende": times_end[num], "Beschreibung": descriptions[num],
+             "Preis": costs[num], "Link": links[num], "Ort": addresses[num]})
+
+    print("Starting Search")
+    return jsonify(res)
 
 @app.route("/api/search")
 def api():
     therm = request.args.get("therm")
-    print("Starting Search")
-    print(therm)
-    return f"{therm} is the therm"
+
+    return jsonify({
+        "Response": therm,
+        "Code": 200
+    })
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
--- suche.py
+++ suche.py
@@ -1,3 +1,52 @@
+import json
+
+def read_data():
+	data_koeln = "WebScrapping/Kölnopendata.json"
+
+	with open(data_koeln, "r", encoding="utf-8")as data_r:
+		read_data = data_r.read()
+	data = json.loads(read_data)
+
+	count = 0
+	try:
+		while True:
+			title = data['items'][count]['title']
+			count += 1
+	except:
+		print(count)
+
+	titles = []
+	times_start = []
+	times_end = []
+	descriptions = []
+	links = []
+	costs = []
+	addresses = []
+
+	for num in range(count):
+		title = data['items'][num]['title']
+		time_start = data['items'][num]['beginndatum']
+		time_end = data['items'][num]["endedatum"]
+		description = data['items'][num]["description"]
+		link = data['items'][num]['link']
+		cost = data['items'][num]['preis']
+		plz = data['items'][num]['plz']
+		ort = data['items'][num]['ort']
+		street = data['items'][num]['strasse']
+		number = data['items'][num]['hausnummer']
+		descriptions.append(description)
+		costs.append(cost)
+		links.append(link)
+		addresses.append(f"{street} {number}, {plz} {ort}")
+		times_start.append(time_start)
+		times_end.append(time_end)
+		titles.append(title)
+
+	return [times_start, times_end, titles, descriptions, costs, links, addresses]
+
+
+
+
 ## searching function
 def search_element(arr, n, element):
 
@@ -17,10 +66,12 @@ if __name__ == '__main__':
 	## initializing the array, length, and element to be searched
 	arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	n = 10
-	element_to_be_searched = 6
+	element_to_be_searched = 10
 	# element_to_be_searched = 11
 
 	if search_element(arr, n, element_to_be_searched):
 		print(element_to_be_searched, "is found")
 	else:
 		print(element_to_be_searched, "is not found")
+
+	read_data()

92e187b0403e5b51db7c33de866c493806e1a115
unibara <unibara@proton.me>
2023-09-16 22:40:56 +0200

    dockerfile für das frontend + fixes

--- frontend-neu/src/routes/+page.svelte
+++ frontend-neu/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import SettingsIcon from '$lib/settings.svelte';
+	import SettingsIcon from '$lib/icons/settings.svelte';
 	import EventCard from './EventCard.svelte';
 
 	interface Event {

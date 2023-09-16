57aa2aec2c93d0dce205e306b61ae6163b58fa7c
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-16 14:56:58 +0200

    automatic cards

--- frontend-neu/src/routes/+page.svelte
+++ frontend-neu/src/routes/+page.svelte
@@ -1,12 +1,35 @@
-<script>
+<script lang="ts">
 	import SettingsIcon from '$lib/settings.svelte';
 
-	const BACKEND_SERVER = "http://localhost:5000/"
+	interface Event {
+		Titel: string;
+		Start: string;
+		Ende: string;
+		Ort: string;
+		Link: string;
+		Beschreibung: string;
+		Preis: string;
+	}
+
+	const BACKEND_SERVER = 'http://192.168.53.46:5000/';
 
 	let search_text = '';
+	let search_results: Event[] = [
+		{
+			Titel: 'Test',
+			Start: '2021-06-01T00:00:00',
+			Ende: '2021-06-01T00:00:00',
+			Ort: 'Test',
+			Link: 'Test',
+			Beschreibung: 'Test',
+			Preis: 'Test'
+		}
+	];
 
 	async function search() {
-
+		const response = await fetch(`${BACKEND_SERVER}/api/search?therm=${search_text}`);
+		const data = await response.json();
+		search_results = data;
 	}
 </script>
 
@@ -22,4 +45,17 @@
 
 	<button on:click={search}>Suchen</button>
 
+	{#each search_results as result}
+		<article>
+			<hgroup>
+				<h2>{result.Titel}</h2>
+				<h3><a href={result.Link}>{result.Link}</a></h3>
+			</hgroup>
+			<p>{result.Beschreibung}</p>
+
+			<a href="#" role="button" class="secondary">Anschauen</a>
+
+			<footer>{result.Ort} • {result.Start} • {result.Preis}</footer>
+		</article>
+	{/each}
 </main>

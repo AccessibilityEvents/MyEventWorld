5e4b63c434ffa3861e8d7a0e2b80a1b35cea98b6
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-17 01:27:04 +0200

    category selection

--- frontend-neu/src/routes/+page.svelte
+++ frontend-neu/src/routes/+page.svelte
@@ -2,11 +2,12 @@
 	import { onMount } from 'svelte';
 	import SettingsIcon from '$lib/icons/settings.svelte';
 	import EventCard from './EventCard.svelte';
-	import type { Event } from '$lib/event.ts'
+	import type { Event } from '$lib/event.ts';
 
 	const BACKEND_SERVER = 'http://192.168.167.46:5000/';
 
-	let search_text = '';
+	let search_text: string = '';
+	let category: string = '';
 	let search_results: Event[] = [];
 	let loading: boolean = false;
 
@@ -17,7 +18,7 @@
 		if (search_text === '') {
 			search_route = 'all';
 		} else {
-			search_route = `search?therm=${search_text}`;
+			search_route = `search?therm=${search_text}&category=${category}`;
 		}
 
 		const response = await fetch(`${BACKEND_SERVER}/api/${search_route}`);
@@ -41,22 +42,22 @@
 	<h1>MyEventWorld</h1>
 
 	<form>
-		<input type="search" bind:value={search_text} placeholder="Suche" autofocus />
-
-		<!-- <div style="display: flex; align-items: center; gap: 2rem;">
-			<a href="/settings">
-				<button class="outline">
-					<SettingsIcon /> Hi
-				</button>
-			</a>
-			<p>Hi</p>
-		</div> -->
-		<!--
-		<a href="/settings">
-			<button style="width: 10%;" class="outline">
-				<SettingsIcon />
-			</button>
-		</a> -->
+		<div class="grid">
+			<input type="search" bind:value={search_text} placeholder="Suche" autofocus />
+			<select name="Kategorie" id="kategorie" bind:value={category}>
+				<option>MINT</option>
+ 				<option>Politik</option>
+				<option>Bürgerbeteiligung</option>
+				<option>Sprache</option>
+				<option>Handwerk</option>
+				<option>Informatik</option>
+				<option>Kunst/Kultur</option>
+				<option>Geschichte</option>
+				<option>Logikspiele/Spiele</option>
+				<option>Sport</option>
+				<option>Schule</option>
+			</select>
+		</div>
 
 		{#if !loading}
 			<button type="submit" on:click|preventDefault={search}>Suchen</button>

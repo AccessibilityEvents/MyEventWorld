b00c7949b48cea8a6dc8ce62ad9f02408a7c1d67
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-16 11:33:51 +0200

    remove unused code

--- server.py
+++ server.py
@@ -24,10 +24,5 @@ def home(path):
     return send_from_directory("frontend/dist", path)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")

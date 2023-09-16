2e66ff28fd085356479b137ed4c35402acf22fd4
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-16 17:58:45 +0200

    temp improve search

--- frontend-neu/src/routes/+page.svelte
+++ frontend-neu/src/routes/+page.svelte
@@ -18,50 +18,43 @@
 	let search_text = '';
 	let search_results: Event[] = [];
 	let loading: boolean = false;
-	let input_error: boolean = false;
 
 	async function search() {
-		if (search_text === "") {
-			input_error = true;
-			return
-		}
-
-		input_error = false;
 		loading = true;
 
-		const response = await fetch(`${BACKEND_SERVER}/api/search?therm=${search_text}`);
+		let search_route: string;
+		if (search_text === "") {
+			search_route = "all";
+		} else {
+			search_route = `search?therm=${search_text}`
+		}
+
+		const response = await fetch(`${BACKEND_SERVER}/api/${search_route}`);
 		const data = await response.json();
-		search_results = data;
+
+		if (!data.Code) {
+			search_results = data;
+		} else {
+			search_results = [];
+		}
 
 		loading = false;
 	}
 
-	onMount(async () => {
-		loading = true;
-
-		const response = await fetch(`${BACKEND_SERVER}/api/all`);
-		const data = await response.json();
-		search_results = data;
-
-		loading = false;
-	});
+	// onMount(async () => { search()});
 </script>
 
 <main class="container">
 	<h1>MyEventWorld</h1>
 
 	<form>
-		{#if input_error}
-			<input type="search" aria-invalid="true" bind:value={search_text} placeholder="Suche" autofocus/>
-		{:else}
-			<input type="search" bind:value={search_text} placeholder="Suche" autofocus/>
-		{/if}
-
+		<input type="search" bind:value={search_text} placeholder="Suche" autofocus/>
+<!--
 		<a href="/settings">
 			<button style="width: 10%;" class="outline">
 				<SettingsIcon />
 			</button>
-		</a>
+		</a> -->
 
 		{#if !loading}
 			<button type="submit" on:click|preventDefault={search}>Suchen</button>

b4937ebee831a72caf413ca0ba5f77e444e4255d
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-16 13:56:23 +0200

    start page

--- frontend-neu/src/routes/+page.svelte
+++ frontend-neu/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import SettingsIcon from '$lib/settings.svelte';
+
+	const BACKEND_SERVER = "http://localhost:5000/"
+
+	let search_text = '';
+
+	async function search() {
+
+	}
+</script>
+
+<main class="container">
+	<h1>MyEventWorld</h1>
+
+	<input type="search" bind:value={search_text} />
+	<a href="/sidebar">
+		<button style="width: 10%;" class="outline">
+			<SettingsIcon />
+		</button>
+	</a>
+
+	<button on:click={search}>Suchen</button>
+
+</main>

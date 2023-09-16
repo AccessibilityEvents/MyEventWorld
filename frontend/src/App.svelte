d2b3ddd635de1173b83e894cfbf88d714c33e931
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2023-09-16 12:17:52 +0200

    app.svelte durch siteMenue ersetzt

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,46 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  //import { onMount, reactive } from "svelte";
+  let ort = "";
+  let distanz = 0;
+  // let filter = reactive({});
+
+  const updateFilter = (event) => {
+    filter[event.target.name] = event.target.checked;
+  };
+
+  // onMount(async () => {
+  //     map = new google.maps.Map(document.getElementById("map"), {
+  //       center: { lat: -34.397, lng: 150.644 },
+  //       zoom: 8,
+  //     });
+  //  });
 </script>
 
-<main>
-  <h1>Hello World</h1>
-</main>
+<div>
+  <label for="ort">Ort:</label>
+  <input id="ort" bind:value={ort} type="text" placeholder="Ort eingeben" />
+</div>
+
+<div>
+  <label for="distanz">Distanz:</label>
+  <input id="distanz" bind:value={distanz} type="range" min="0" max="100" />
+</div>
+
+<div>
+  <h2>Filter für Veranstaltungen</h2>
+  <label>
+    <input type="checkbox" name="filter1" on:change={updateFilter} />
+    Filter1
+  </label>
+  <label>
+    <input type="checkbox" name="filter2" on:change={updateFilter} />
+    Filter2
+  </label>
+</div>
+
+<style>
+  #map {
+    height: 300px;
+    width: 100%;
+  }
+</style>

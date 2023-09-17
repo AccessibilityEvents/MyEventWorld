1781e5b7cade35103646317bf7ca5d81d494f26c
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-17 02:04:32 +0200

    add to calendar

--- frontend-neu/src/routes/EventCard.svelte
+++ frontend-neu/src/routes/EventCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Event } from '$lib/event.ts'
+	import type { Event } from '$lib/event.ts';
 	// import EventCardModal from './EventCardModal.svelte';
 	import EuroIcon from '$lib/icons/euro.svelte';
 	import MapPinIcon from '$lib/icons/map-pin.svelte';
@@ -15,6 +15,19 @@
 	<p>{event.Beschreibung}</p>
 
 	<!-- <EventCardModal title={titel} description={beschreibung} link_to_event={link} time={start} /> -->
+	<add-to-calendar-button
+		name="{event.Titel}"
+		description="{event.Beschreibung}"
+		startDate="{event.Start}"
+		endDate="{event.Ende}"
+		location="{event.Ort}"
+		options="['Apple','Google','iCal','Microsoft365','Outlook.com','Yahoo']"
+		timeZone="Europe/Berlin"
+		trigger="click"
+		inline
+		listStyle="modal"
+		iCalFileName="Reminder-Event"
+	/>
 
 	<footer>
 		<EuroIcon />
